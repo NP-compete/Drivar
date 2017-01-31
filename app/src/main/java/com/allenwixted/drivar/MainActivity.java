8eb688cabe5c2c1599664e7113cc7609505d6a78
Allen Wixted <allenwixted@gmail.com>
2017-01-31 14:44:18 +0000

    Optimised the algorithm for delaying requests.

--- app/src/main/java/com/allenwixted/drivar/MainActivity.java
+++ app/src/main/java/com/allenwixted/drivar/MainActivity.java
@@ -35,6 +35,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Objects;
 
 import static java.lang.Math.round;
 
@@ -51,11 +52,13 @@ public class MainActivity extends AppCompatActivity {
     View speedLimitCircle;
     private int lmDistance = 5;
     private int lmTime = 250;
-    private int speedLimit = 30;
+    private int speedLimit = 50;
     private int usersSpeed = 0;
     private int drivingRecommendedSpeed = 30;
     private boolean weatherBad = false;
     private double metricImperial = 3.6;
+    private Location previousLocation = null;
+    private double distance = 0;
 
     public double scale(double valueIn, double baseMin, double baseMax, double limitMin, double limitMax) {
         return ((limitMax - limitMin) * (valueIn - baseMin) / (baseMax - baseMin)) + limitMin;
@@ -97,10 +100,8 @@ public class MainActivity extends AppCompatActivity {
 
         currentSpeedView.setScaleX(scaledScreenWidthForCSV);
         currentSpeedView.setScaleY(scaledScreenWidthForCSV);
-
         recommendedSpeed.setScaleX(scaledScreenWidthForRSV);
         recommendedSpeed.setScaleY(scaledScreenWidthForRSV);
-
         speedLimitCircle.setScaleX(scaledScreenWidthForRSV);
         speedLimitCircle.setScaleY(scaledScreenWidthForRSV);
 
@@ -142,11 +143,11 @@ public class MainActivity extends AppCompatActivity {
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                String speed = String.valueOf((int) Math.round(location.getSpeed()*3.6));
-                usersSpeed = (int) Math.round(location.getSpeed()*3.6);
+                String speed = String.valueOf((int) Math.round(location.getSpeed()*metricImperial));
+                usersSpeed = (int) Math.round(location.getSpeed()*metricImperial);
                 text.setText(speed);
 
-                double scaledSpeed = scale((int) Math.round(location.getSpeed()*3.6), 0, speedLimit, 1, 2.5);
+                double scaledSpeed = scale((int) Math.round(location.getSpeed()*metricImperial), 0, speedLimit, 1, 2.5);
 
                 currentSpeedView.animate().scaleX((float) scaledSpeed).scaleY((float) scaledSpeed).setDuration(250);
 
@@ -155,51 +156,67 @@ public class MainActivity extends AppCompatActivity {
                 String lat = String.valueOf(location.getLatitude());
                 String lng = String.valueOf(location.getLongitude());
 
-                //Speed URL
-                SpeedLimitRequest task = new SpeedLimitRequest();
-                String speedURL = "https://route.cit.api.here.com/routing/7.2/getlinkinfo.json?waypoint=" +
-                        lat +
-                        "%2C" +
-                        lng +
-                        "&app_id=" +
-                        "F95P9Ir1dbtspwKRTKtE" +
-                        "&app_code=" +
-                        "VtVWqzE5hlQfoozVs1jujA";
-                task.execute(String.valueOf(speedURL));
+                //Delay JSON Requests
+                // map current speed from 0km/h - 130km/h to 100m - 2000m
+                int distanceMap = (int) scale(usersSpeed, 0, 130, 10, 1000);
+                Log.i(TAG, String.valueOf(distanceMap));
 
-                //Weather URL
-                WeatherRequest weatherRequest = new WeatherRequest();
-                String weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat +
-                        "&lon=" + lng +
-                        "&appid=b19ba30c78778b7a632b1c8c15c51747";
-                Log.i(TAG, weatherURL);
-                weatherRequest.execute(weatherURL);
 
-                TwilightCalculator dayNight = new TwilightCalculator();
-                long time= System.currentTimeMillis();
-                dayNight.calculateTwilight(time, location.getLatitude(), location.getLongitude());
+                if (previousLocation != null) {
+                    //calculate distance
+                    distance = distance + location.distanceTo(previousLocation);
+                    Log.i(TAG, String.valueOf(distance) + " distance travelled");
+                    if(distance > distanceMap){
+                        distance = 0;
 
-                if(dayNight.mState == 1) {
-                    Log.i("DAY / NIGHT", "it is night and clear");
-                    recommendedSpeed.animate().alpha(255).setDuration(10000);
-                    recommendedSpeed.animate().scaleX((float) 0.9).scaleY((float) 0.9).setDuration(300);
-                    drivingRecommendedSpeed = (int) (speedLimit * 0.9);
-                } else if (dayNight.mState == 0) {
-                    Log.i("DAY / NIGHT", "it is day and clear");
-                    recommendedSpeed.animate().alpha(0).setDuration(10000);
-                    recommendedSpeed.animate().scaleX((float) 1.0).scaleY((float) 1.0).setDuration(300);
-                    drivingRecommendedSpeed = speedLimit;
-                } else if (dayNight.mState == 1 && weatherBad == true){
-                    Log.i("DAY / NIGHT", "it is night and bad weather");
-                    recommendedSpeed.animate().alpha(255).setDuration(10000);
-                    recommendedSpeed.animate().scaleX((float) 0.8).scaleY((float) 0.8).setDuration(300);
-                    drivingRecommendedSpeed = (int) (speedLimit * 0.8);
-                } else if (dayNight.mState == 0 && weatherBad == true){
-                    Log.i("DAY / NIGHT", "it is day and bad weather");
-                    recommendedSpeed.animate().alpha(255).setDuration(10000);
-                    recommendedSpeed.animate().scaleX((float) 0.9).scaleY((float) 0.9).setDuration(300);
-                    drivingRecommendedSpeed = (int) (speedLimit * 0.9);
+                        //Speed URL
+                        SpeedLimitRequest task = new SpeedLimitRequest();
+                        String speedURL = "https://route.cit.api.here.com/routing/7.2/getlinkinfo.json?waypoint=" +
+                                lat +
+                                "%2C" +
+                                lng +
+                                "&app_id=" +
+                                "F95P9Ir1dbtspwKRTKtE" +
+                                "&app_code=" +
+                                "VtVWqzE5hlQfoozVs1jujA";
+                        task.execute(String.valueOf(speedURL));
+
+                        //Weather URL
+                        WeatherRequest weatherRequest = new WeatherRequest();
+                        String weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat +
+                                "&lon=" + lng +
+                                "&appid=b19ba30c78778b7a632b1c8c15c51747";
+                        Log.i(TAG, speedURL);
+                        weatherRequest.execute(weatherURL);
+
+                        TwilightCalculator dayNight = new TwilightCalculator();
+                        long time= System.currentTimeMillis();
+                        dayNight.calculateTwilight(time, location.getLatitude(), location.getLongitude());
+
+                        if(dayNight.mState == 1) {
+                            Log.i("DAY / NIGHT", "it is night and clear");
+                            recommendedSpeed.animate().alpha(255).setDuration(10000);
+                            recommendedSpeed.animate().scaleX((float) 0.9).scaleY((float) 0.9).setDuration(300);
+                            drivingRecommendedSpeed = (int) (speedLimit * 0.9);
+                        } else if (dayNight.mState == 0) {
+                            Log.i("DAY / NIGHT", "it is day and clear");
+                            recommendedSpeed.animate().alpha(0).setDuration(10000);
+                            recommendedSpeed.animate().scaleX((float) 1.0).scaleY((float) 1.0).setDuration(300);
+                            drivingRecommendedSpeed = speedLimit;
+                        } else if (dayNight.mState == 1 && weatherBad == true){
+                            Log.i("DAY / NIGHT", "it is night and bad weather");
+                            recommendedSpeed.animate().alpha(255).setDuration(10000);
+                            recommendedSpeed.animate().scaleX((float) 0.8).scaleY((float) 0.8).setDuration(300);
+                            drivingRecommendedSpeed = (int) (speedLimit * 0.8);
+                        } else if (dayNight.mState == 0 && weatherBad == true){
+                            Log.i("DAY / NIGHT", "it is day and bad weather");
+                            recommendedSpeed.animate().alpha(255).setDuration(10000);
+                            recommendedSpeed.animate().scaleX((float) 0.9).scaleY((float) 0.9).setDuration(300);
+                            drivingRecommendedSpeed = (int) (speedLimit * 0.9);
+                        }
+                    }
                 }
+                previousLocation = location;
             }
 
             @Override
@@ -234,6 +251,14 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_LONG;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+
+        CharSequence feedback = "We made some changes to the app....";
+        Toast toast2 = Toast.makeText(context, feedback, duration);
+        toast2.show();
+
+        CharSequence feedback2 = "Let us know if the speed limit doesn't update quick enough.";
+        Toast toast3 = Toast.makeText(context, feedback2, duration);
+        toast3.show();
     }
 
     private void calculateUIColor() {
@@ -319,41 +344,48 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
+            Log.i(TAG, "RESULT " + result);
 
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                String response = jsonObject.getString("response");
-                try{
-                    JSONObject linkJSON = new JSONObject(response);
-                    String link = linkJSON.getString("link");
+            if(result != null && !Objects.equals(result, "")){
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    if (jsonObject.getString("response") != null) {
 
-                    JSONArray jsonArray = new JSONArray(link);
+                        String response = jsonObject.getString("response");
+                        try {
+                            JSONObject linkJSON = new JSONObject(response);
+                            String link = linkJSON.getString("link");
+                            try {
+                                JSONArray jsonArray = new JSONArray(link);
 
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jsonPart = jsonArray.getJSONObject(i);
-                        speedLimit = (int) Math.round(jsonPart.getDouble("speedLimit")*3.6);
+                                if (jsonArray.length() > 0) {
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject jsonPart = jsonArray.getJSONObject(i);
+                                        speedLimit = (int) Math.round(jsonPart.getDouble("speedLimit") * metricImperial);
+                                        if (speedLimit != 0) {
+                                            speed.setText(String.valueOf(speedLimit));
+                                        } else {
+                                            speed.setText("?");
+                                        }
+                                    }
+                                } else {
+                                    speed.setText("?");
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
 
-                        if(speedLimit != 0){
-                            speed.setText(String.valueOf(speedLimit));
-                        } else {
-                            speed.setText("?");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-
-                        //Log.i("JSON Speed Limit", String.valueOf((speedLimit)));
+                    } else {
+                        speed.setText("?");
                     }
+                }catch(JSONException e){
 
-                } catch (JSONException e){
-                    Context context = getApplicationContext();
-                    CharSequence text = "Error getting Speed Limit. Try Updating";
-                    int duration = Toast.LENGTH_LONG;
-
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
                 }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
             }
+
         }
     }
 
@@ -394,55 +426,58 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
 
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                String response = jsonObject.getString("weather");
+            if(result != null && !Objects.equals(result, "")){
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    String response = jsonObject.getString("weather");
+
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jsonPart = jsonArray.getJSONObject(i);
+                            int code = jsonPart.getInt("id");
+
+                            if (code > 799 && code < 899) {
+                                weatherBad = false;
+                            } else {
+                                weatherBad = true;
+                            }
+                        }
+
+                    } catch (JSONException e) {
+                        Context context = getApplicationContext();
+                        CharSequence text = "Error getting Weather Data. Try Updating";
+                        int duration = Toast.LENGTH_LONG;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                } catch (JSONException e) {
+
+                }
 
                 try {
-                    JSONArray jsonArray = new JSONArray(response);
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jsonPart = jsonArray.getJSONObject(i);
-                        int code = jsonPart.getInt("id");
+                    JSONObject jsonObject = new JSONObject(result);
+                    String response = jsonObject.getString("sys");
 
-                        if (code > 799 && code < 899) {
-                            weatherBad = false;
-                        } else {
-                            weatherBad = true;
+                    try{
+                        if(response != null) {
+                            JSONObject jsonPart = new JSONObject(response);
+                            String country = jsonPart.getString("country");
+
+                            if (country.equals("US") || country.equals("UK")) {
+                                metricImperial = 2.23;
+                            } else {
+                                metricImperial = 3.6;
+                            }
                         }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-
-                } catch (JSONException e) {
-                    Context context = getApplicationContext();
-                    CharSequence text = "Error getting Weather Data. Try Updating";
-                    int duration = Toast.LENGTH_LONG;
-
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                }
-            } catch (JSONException e) {
-
-            }
-
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                String response = jsonObject.getString("sys");
-
-                try{
-                    JSONObject jsonPart = new JSONObject(response);
-                    String country = jsonPart.getString("country");
-                    Log.i(TAG, country);
-
-                    if(country.equals("US") || country.equals("UK")){
-                        metricImperial = 2.23;
-                    } else {
-                        metricImperial = 3.6;
-                    }
-
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                } catch (JSONException e) {
-                     e.printStackTrace();
             }
         }
     }
